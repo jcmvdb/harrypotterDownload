287d4b6b662a899954a2069542cd33695c596586
jcmvdb <mvdb.jeroen@gmail.com>
2022-09-11 22:29:03 +0200

    basic

---
+++ hp1.php
@@ -0,0 +1,7 @@
+<title>Harry Potter 1 pc game</title>
+<?php
+    include "header.php";
+
+    echo "<h1>Harry Potter 1 pc game - download</h1>";
+
+    include "footer.php";
---
+++ hp2.php
@@ -0,0 +1,7 @@
+<title>Harry Potter 2 pc game</title>
+<?php
+include "header.php";
+
+echo "<h1>Harry Potter 2 pc game - download</h1>";
+
+include "footer.php";
